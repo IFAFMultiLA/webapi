3296a42b6205b022cf700a41d7b2ab78b7b60af8
Markus Konrad <post@mkonrad.net>
2023-03-21 10:11:57 +0100

    WIP

--- src/api/urls.py
+++ src/api/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('session/', views.app_session),
+    path('session_login/', views.app_session_login),
 ]
--- src/api/views.py
+++ src/api/views.py
@@ -1,32 +1,22 @@
 from django.http import HttpResponse, JsonResponse
+from django.shortcuts import get_object_or_404
 
-from .models import ApplicationSession, ApplicationConfig, UserApplicationSession
+from .models import ApplicationSession, ApplicationConfig, UserApplicationSession, User
 
 
 def app_session(request):
     if request.method == 'GET':
         sess_code = request.GET.get('sess', None)
         if sess_code:
-            try:
-                sess_obj = ApplicationSession.objects.get(code=sess_code)
-            except ApplicationSession.DoesNotExist:
-                return HttpResponse(status=404)
+            app_sess_obj = get_object_or_404(ApplicationSession, code=sess_code)
+            response_data = {'sess_code': app_sess_obj.code, 'auth_mode': app_sess_obj.auth_mode}
 
-            response_data = {'sess_code': sess_obj.code, 'auth_mode': sess_obj.auth_mode}
-
-            if sess_obj.auth_mode == 'none':
-                try:
-                    app_config = ApplicationConfig.objects.get(id=sess_obj.config_id)
-                except ApplicationConfig.DoesNotExist:
-                    return HttpResponse(status=404)
-
-                user_sess_obj = UserApplicationSession(application_session=sess_obj)  # user_id will stay None
-                user_sess_code = user_sess_obj.generate_code()
-                user_sess_obj.save(force_insert=True)
+            if app_sess_obj.auth_mode == 'none':
+                app_config_obj, user_sess_obj = _generate_user_session(app_sess_obj)    # user_id will stay None
 
                 response_data.update({
-                    'user_code': user_sess_code,
-                    'config': app_config.config
+                    'user_code': user_sess_obj.code,
+                    'config': app_config_obj.config
                 })
 
                 status = 201
@@ -35,4 +25,37 @@ def app_session(request):
 
             return JsonResponse(response_data, status=status)
 
-    return HttpResponse(status=401)
+    return HttpResponse(status=400)
+
+
+def app_session_login(request):
+    if request.method == 'POST':
+        sess_code = request.POST.get('sess', None)
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+
+        if sess_code and username and password:
+            app_sess_obj = get_object_or_404(ApplicationSession, code=sess_code)
+            user_obj = get_object_or_404(User, username=username)
+
+            if user_obj.check_password(password):
+                app_config_obj, user_sess_obj = _generate_user_session(app_sess_obj, user_obj)
+
+                return JsonResponse({
+                    'sess_code': app_sess_obj.code,
+                    'auth_mode': app_sess_obj.auth_mode,
+                    'user_code': user_sess_obj.code,
+                    'config': app_config_obj
+                }, status=201)
+
+    return HttpResponse(status=400)
+
+
+def _generate_user_session(app_sess_obj, user_obj=None):
+    app_config_obj = get_object_or_404(ApplicationConfig, id=app_sess_obj.config_id)
+
+    user_sess_obj = UserApplicationSession(application_session=app_sess_obj, user=user_obj)
+    user_sess_obj.generate_code()
+    user_sess_obj.save(force_insert=True)
+
+    return app_config_obj, user_sess_obj
