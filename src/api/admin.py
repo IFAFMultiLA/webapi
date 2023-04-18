99c1e2d1055e09d3ffcde74752d552a1dd8e1c50
Markus Konrad <post@mkonrad.net>
2023-04-18 16:15:33 +0200

    WIP

--- src/api/admin.py
+++ src/api/admin.py
@@ -3,9 +3,11 @@ Admin backend.
 
 ModelAdmin classes to generate the django administration backend.
 """
+from collections import defaultdict
 
-
+from django import forms
 from django.contrib import admin
+from django.db.models import Count, Max, Avg, F
 from django.template.response import TemplateResponse
 from django.urls import path, reverse
 from django.utils.safestring import mark_safe
@@ -119,11 +121,99 @@ class MultiLAAdminSite(admin.AdminSite):
         return app_list
 
     def dataview(self, request):
+        def default_format(v):
+            return '-' if v is None else v
+
+        def format_timedelta(t):
+            if t is None:
+                return default_format(t)
+            return f'{int(t.total_seconds() // 60)}min {round((t.total_seconds() / 60 - t.total_seconds() // 60) * 60)}s'
+
+        def format_datetime(t):
+            if t is None:
+                return default_format(t)
+            return t.strftime('%Y-%m-%d %H:%M:%S')
+
+        CONFIGFORM_GROUPBY_CHOICES = [
+            ('app', 'Application'),
+            ('app_config', 'Application config.'),
+            ('app_session', 'Application session')
+        ]
+
+        COLUMN_DESCRIPTIONS = {
+            'n_users': 'Total num. users',
+            'n_nonanon_users': 'Registered users',
+            'n_nonanon_logins': 'Logins of registered users',
+            'n_trackingsess': 'Num. tracking sessions',
+            'most_recent_trackingsess': 'Most recently started tracking session',
+            'avg_trackingsess_duration': 'Avg. tracking session duration',
+            'n_events': 'Num. recorded tracking events',
+            'most_recent_event': 'Most recently recorded event'
+        }
+
+        COLUMN_FORMATING = {
+            'avg_trackingsess_duration': format_timedelta,
+            'most_recent_trackingsess': format_datetime,
+            'most_recent_event': format_datetime
+        }
+
+        class ConfigForm(forms.Form):
+            groupby = forms.ChoiceField(label='Group by', choices=CONFIGFORM_GROUPBY_CHOICES, required=False)
+
+        if request.method == 'POST':
+            configform = ConfigForm(request.POST)
+
+            if configform.is_valid():
+                request.session['dataview_configform'] = configform.cleaned_data
+        else:
+            configform = ConfigForm(request.session.get('dataview_configform', {}))
+
+        viewconfig = request.session.get('dataview_configform', {})
+        groupby = viewconfig.get('groupby', CONFIGFORM_GROUPBY_CHOICES[0][0])
+
+        if groupby == 'app':
+            usersess_expr = 'applicationconfig__applicationsession__userapplicationsession'
+            trackingsess_expr = usersess_expr + '__trackingsession'
+            trackingevent_expr = trackingsess_expr + '__trackingevent'
+
+            data_fields = ['name', 'url', 'n_users', 'n_nonanon_users', 'n_nonanon_logins', 'n_trackingsess',
+                           'most_recent_trackingsess', 'avg_trackingsess_duration', 'n_events', 'most_recent_event']
+            data_rows = Application.objects\
+                .annotate(n_users=Count(usersess_expr, distinct=True),
+                          n_nonanon_users=Count(usersess_expr + '__user', distinct=True),
+                          n_nonanon_logins=Count(usersess_expr + '__user', distinct=False),
+                          n_trackingsess=Count(trackingsess_expr, distinct=True),
+                          most_recent_trackingsess=Max(trackingsess_expr + '__start_time'),
+                          avg_trackingsess_duration=Avg(F(trackingsess_expr + '__end_time')
+                                                        - F(trackingsess_expr + '__start_time')),
+                          n_events=Count(trackingevent_expr, distinct=True),
+                          most_recent_event=Max(trackingevent_expr + '__time'))\
+                .order_by('name').values(*data_fields)
+        elif groupby == 'app_config':
+            # TODO
+            data_fields = []
+            data_rows = []
+        elif groupby == 'app_session':
+            # TODO
+            data_fields = []
+            data_rows = []
+        else:
+            raise ValueError(f'invalid value for "groupby": {groupby}')
+
+        table_data = defaultdict(list)
+        for row in data_rows:
+            formatted_row = [COLUMN_FORMATING.get(k, default_format)(row[k])
+                             for k in data_fields if k not in {'name', 'url'}]
+            table_data[(row['name'], row['url'])].append(formatted_row)
+
         context = {
             **self.each_context(request),
             "title": self.index_title,
             "subtitle": "Data view",
             "app_label": 'dataview',
+            "configform": configform,
+            "table_columns": [COLUMN_DESCRIPTIONS[k] for k in data_fields if k in COLUMN_DESCRIPTIONS],
+            "table_data": table_data.items()
         }
 
         request.current_app = self.name
