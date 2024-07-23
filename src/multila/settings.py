62ca46d7ffd48a7ad68119dacea16183ff17657d
Markus Konrad <post@mkonrad.net>
2024-07-23 08:45:55 +0200

    GH actions WIP

--- src/multila/settings.py
+++ src/multila/settings.py
@@ -148,5 +148,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if DEBUG:
     import socket  # only if you haven't already imported this
-    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
-    INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
+    try:
+        hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+        INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
+    except socket.gaierror:
+        hostname = 'localhost'
+        INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
